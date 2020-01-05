196a3b282af13e7100753320493c10c4b3c53f83
AkeNygren <ake@nygren.ch>
2020-01-05 15:50:14 +0100

    sadf

--- main.py
+++ main.py
@@ -32,4 +32,5 @@ while True:
     if currtime.second == 0:
         print("in here")
         im_idx = np.random.randint(0, len(im_array))
-        led_display.scroll_matrix(matrix=im_array[im_idx], sweeps=10, speed=100)
+        #led_display.scroll_matrix(matrix=im_array[im_idx], sweeps=10, speed=100)
+        led_display.display_matrix(mat=im_array[im_idx],steps=20)
