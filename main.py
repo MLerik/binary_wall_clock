1f76685932f8133c59ef32accfa01356b781def3
AkeNygren <ake@nygren.ch>
2020-01-05 15:47:22 +0100

    added comments to functions

--- main.py
+++ main.py
@@ -30,5 +30,6 @@ while True:
 
     currtime = datetime.datetime.now()
     if currtime.second == 0:
+        print("in here")
         im_idx = np.random.randint(0, len(im_array))
         led_display.scroll_matrix(matrix=im_array[im_idx], sweeps=1, speed=1)
