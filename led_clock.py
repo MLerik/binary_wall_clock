6a77251439f82341901624ea37ed7e42f5ec66f6
AkeNygren <ake@nygren.ch>
2020-01-05 15:42:46 +0100

    added comments to functions

--- led_clock.py
+++ led_clock.py
@@ -2,6 +2,7 @@ import datetime
 from gpiozero import LED
 from display import LED_Display
 
+
 class LedClock():
 
     def __init__(self, h_p, m_p, s_p):
@@ -9,11 +10,18 @@ class LedClock():
         self.second_leds = []
         self.minute_leds = []
         self.hour_leds = []
-        self.init_pins(h_p,m_p,s_p)
+        self.init_pins(h_p, m_p, s_p)
         self.display = LED_Display()
         return
 
-    def init_pins(self,h_p,m_p,s_p):
+    def init_pins(self, h_p, m_p, s_p):
+        """
+        Initiate pins on raspberry for IO communication
+        :param h_p:
+        :param m_p:
+        :param s_p:
+        :return:
+        """
         for seconds_pin in s_p:
             self.second_leds.append(LED(seconds_pin))
         for minute_pin in m_p:
@@ -22,8 +30,13 @@ class LedClock():
             self.hour_leds.append(LED(hour_pin))
         return
 
-
     def _update_time(self):
+        """
+        Updates the time of the Binary Clock
+
+        :return:
+        """
+
         curr_time = datetime.datetime.now()
         time = []
         time.append([int(x) for x in '{0:06b}'.format(curr_time.second)])
@@ -31,27 +44,33 @@ class LedClock():
         time.append([int(x) for x in '{0:06b}'.format(curr_time.hour)])
         time.append([int(x) for x in '{0:06b}'.format(curr_time.day)])
         time.append([int(x) for x in '{0:06b}'.format(curr_time.month)])
-        time.append([int(x) for x in '{0:06b}'.format(curr_time.year-2000)])
+        time.append([int(x) for x in '{0:06b}'.format(curr_time.year - 2000)])
         return time
 
-    def display_time(self,display='LEDScreen'):
+    def display_time(self, display='LEDScreen'):
+        """
+        Displays the current time as binary values on the defined display
+
+        :param display: parameter to set the desired display.
+        :return:
+        """
         self.bin_time = self._update_time()
-        wide = False # defines one or two LEDS for display
+        wide = False  # defines one or two LEDS for display
         if display == 'LEDScreen':
             if not wide:
                 for frame_updates in range(30):
                     for time_slot in range(len(self.bin_time)):
-                        for bit in range(len( self.bin_time[time_slot])):
+                        for bit in range(len(self.bin_time[time_slot])):
                             if self.bin_time[time_slot][bit] == 1:
-                                self.display.light_led(6-time_slot,6-bit,0.001)
+                                self.display.light_led(6 - time_slot, 6 - bit, 0.001)
             else:
                 for frame_updates in range(30):
                     for time_slot in range(3):
                         for bit in range(6):
                             if self.bin_time[time_slot][bit] == 1:
-                                coord = 2*time_slot
-                                self.display.light_led(7-coord, 5 - bit, 0.0001)
-                                self.display.light_led(7-coord-1, 5 - bit, 0.0001)
+                                coord = 2 * time_slot
+                                self.display.light_led(7 - coord, 5 - bit, 0.0001)
+                                self.display.light_led(7 - coord - 1, 5 - bit, 0.0001)
 
         else:
             for time_slot in range(3):
@@ -71,5 +90,3 @@ class LedClock():
                         current_leds[bin_position].off()
                     bin_position += 1
         return
-
-
